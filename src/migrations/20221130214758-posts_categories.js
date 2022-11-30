59ad472f2046588d87e41c8f4345fe575dec99a1
Gabriel <gabrielproencaaraujo@gmail.com>
2022-11-30 19:48:31 -0300

    Item 01

---
+++ src/migrations/20221130214758-posts_categories.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {  
+      postId: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+      },
+      category_id: {
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+      },
+
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+  await queryInterface.dropTable('posts_categories');
+  }
+};
